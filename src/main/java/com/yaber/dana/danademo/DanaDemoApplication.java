0a2c69762434629f4b66fa061e1ebf4d062ee599
tangyabo <13316022418@163.com>
2024-04-23 10:13:40 +0800

    初始化项目

---
+++ src/main/java/com/yaber/dana/danademo/DanaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yaber.dana.danademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yaber/dana/danademo/DanaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaber.dana.danademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
